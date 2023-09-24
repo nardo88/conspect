d3787015cc12a5c8bcffdde1971de0d84ae8728c
nardo88 <nardo1988@mail.ru>
2023-09-24 18:23:07 +0300

    add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,27 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ru">
       <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="./favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="./favicon/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="./favicon/favicon-16x16.png"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,700&display=swap"
           rel="stylesheet"
